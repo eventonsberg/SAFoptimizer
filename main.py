55e861839c2688b4ac786ef3670357c04df89f5e
eventonsberg <even-kristian.tonsberg@ffi.no>
2026-01-09 11:34:10 +0100

    Page title updated

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import streamlit as st
 from tab_1 import display_tab_1
 from tab_2 import display_tab_2
 
+st.set_page_config(
+    page_title="SAF optimizer",
+    page_icon=":material/travel:"
+)
+
 tab1, tab2 = st.tabs(["Missilkostnad", "Missilkostnad per kubikk drivstoff"])
 
 with tab1:
